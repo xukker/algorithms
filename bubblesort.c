850c92ad9a8ace109390fb297bd71125ab8ee5ac
2409hw <2409hw@gmail.com>
2022-07-17 18:30:03 -0400

    bubblesort in c

---
+++ bubblesort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void * swap (int* arr, int i, int k) {
+  int tmp = arr[i];
+  arr[i] = arr[k];
+  arr[k] = tmp;
+}
+
+void sort(int* arr, long its) {
+  for (int i=0; i<its-1; i++) {
+    for (int k=i; k<its-i-1; k++) {
+      if (arr[k] > arr[k+1]) {
+        swap(arr, k, k+1);
+      }
+    }
+  }
+}
+
+void print(int* arr, long its) {
+  for (int i=0; i<its; i++) {
+    printf("%d ", arr[i]);
+  }
+}
+
+int main() {
+  int arr[] = {1,8,3,4,5};
+  size_t its = sizeof(arr)/sizeof(*arr);
+  sort(arr, its);
+  print(arr, its);
+  return 0;
+}
